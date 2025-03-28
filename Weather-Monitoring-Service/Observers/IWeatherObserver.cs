d431627ca816a80ec245e5d727ff6f1a6a79d7ae
Ahmad <nazzala8@gmail.com>
2025-03-28 18:24:39 +0300

    Add Bots Observers

---
+++ Weather-Monitoring-Service/Observers/IWeatherObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather_Monitoring_Service.models;
+
+namespace Weather_Monitoring_Service.Observers
+{
+    public interface IWeatherObserver
+    {
+        public void Update(WeatherData data);
+    }
+}
---
+++ Weather-Monitoring-Service/Observers/RainBot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather_Monitoring_Service.models;
+
+namespace Weather_Monitoring_Service.Observers
+{
+    public class RainBot : BotSettings, IWeatherObserver
+    {
+        public required int HumidityThreshold { get; set; }
+        public void Update(WeatherData data)
+        {
+            if (data.Humidity > HumidityThreshold)
+                Console.WriteLine(Message);
+        }
+    }
+}
