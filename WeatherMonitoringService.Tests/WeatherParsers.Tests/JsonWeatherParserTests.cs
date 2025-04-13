3128eb6f39fa8f9cd7d47b94782ea8bebbff5424
Ahmad <nazzala8@gmail.com>
2025-04-13 23:36:09 +0300

    Add tests for weather parsers

---
+++ WeatherMonitoringService.Tests/WeatherParsers.Tests/JsonWeatherParserTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Weather_Monitoring_Service.models;
+using Weather_Monitoring_Service.Parser;
+
+namespace WeatherMonitoringService.Tests.WeatherParsers.Tests
+{
+    public class JsonWeatherParserTests
+    {
+        private readonly JsonWeatherParser _parser;
+
+        public JsonWeatherParserTests()
+        {
+            _parser = new JsonWeatherParser();
+        }
+
+        [Theory, AutoData]
+        public void JsonWeatherParser_ShouldParseValidJson(WeatherData expected)
+        {
+            string json = JsonSerializer.Serialize(expected);
+
+            var result = _parser.Parse(json);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("{...}")]
+        [InlineData("{\"key\":\"value\"}")]
+        public void JsonWeatherParser_ShouldRecognizeCompatibleData(string json)
+        {
+            _parser.IsCompatible(json).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("not json")]
+        [InlineData("<xml>stuff</xml>")]
+        public void JsonWeatherParser_ShouldRejectIncompatibleData(string data)
+        {
+            _parser.IsCompatible(data).Should().BeFalse();
+        }
+    }
+}
---
+++ WeatherMonitoringService.Tests/WeatherParsers.Tests/XmlWeatherParserTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather_Monitoring_Service.models;
+using Weather_Monitoring_Service.Parser;
+
+namespace WeatherMonitoringService.Tests.WeatherParsers.Tests
+{
+    public class XmlWeatherParserTests
+    {
+        private readonly XmlWeatherParser _parser;
+
+        public XmlWeatherParserTests()
+        {
+            _parser = new XmlWeatherParser();
+        }
+
+        [Theory, AutoData]
+        public void XmlWeatherParser_ShouldParseValidXml(WeatherData expected)
+        {
+            string xml = $"""
+                      <WeatherData>
+                          <Location>{expected.Location}</Location>
+                          <Temperature>{expected.Temperature}</Temperature>
+                          <Humidity>{expected.Humidity}</Humidity>
+                      </WeatherData>
+                      """;
+
+            var result = _parser.Parse(xml);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("<xml>stuff</xml>")]
+        [InlineData("<WeatherData></WeatherData>")]
+        public void XmlWeatherParser_ShouldRecognizeCompatibleData(string xml)
+        {
+            _parser.IsCompatible(xml).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("{\"key\":\"value\"}")]
+        [InlineData("not xml")]
+        public void XmlWeatherParser_ShouldRejectIncompatibleData(string data)
+        {
+            _parser.IsCompatible(data).Should().BeFalse();
+        }
+
+    }
+}
